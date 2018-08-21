a18a5463990b04a5924b81c34c18646b7652a0bb
Aaron Todd <aaron.todd95@gmail.com>
2018-08-21 10:23:21 +0100

    Added StringColumn to optimize string column parsing.

--- src/NeatParser/DateTimeColumn.cs
+++ src/NeatParser/DateTimeColumn.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NeatParser
 {
@@ -16,25 +12,25 @@ namespace NeatParser
         private readonly string format;
 
         /// <summary>
-        /// Constructs a new instance of the <see cref="DateTimeColumn"/> class with specified format.
+        ///     Constructs a new instance of the <see cref="DateTimeColumn" /> class with specified format.
         /// </summary>
         /// <param name="format">Format of the date time string e.g yyyyMMDD</param>
         public DateTimeColumn(string format)
         {
-            if (string.IsNullOrWhiteSpace(format))
+            if(string.IsNullOrWhiteSpace(format))
                 throw new ArgumentNullException(nameof(format));
 
             this.format = format;
         }
 
         /// <summary>
-        /// Constructs a new instance of the <see cref="DateTimeColumn"/> class with specified format and column name.
+        ///     Constructs a new instance of the <see cref="DateTimeColumn" /> class with specified format and column name.
         /// </summary>
         /// <param name="columnName">Name of the column</param>
         /// <param name="format">Format of the date time string e.g yyyyMMDD</param>
         public DateTimeColumn(string columnName, string format) : base(columnName)
         {
-            if (string.IsNullOrWhiteSpace(format))
+            if(string.IsNullOrWhiteSpace(format))
                 throw new ArgumentNullException(nameof(format));
 
             this.format = format;
@@ -46,7 +42,7 @@ namespace NeatParser
             {
                 return DateTime.ParseExact(value, format, CultureInfo.InvariantCulture);
             }
-            catch (Exception ex) when (ex is FormatException || ex is ArgumentNullException)
+            catch(Exception ex) when(ex is FormatException || ex is ArgumentNullException)
             {
                 throw new FileParserException(ex);
             }
---
+++ src/NeatParser/StringColumn.cs
@@ -0,0 +1,28 @@
+﻿namespace NeatParser
+{
+    /// <summary>
+    /// Class for general string columns. Default column. Created for optimization.
+    /// </summary>
+    public class StringColumn : ColumnDefinition<string>
+    {
+        public override bool IsDummy => false;
+
+        public override bool IsLayoutEditor => false;
+
+        /// <summary>
+        ///     Constructs a new instance of the <see cref="StringColumn" /> class.
+        /// </summary>
+        public StringColumn() { }
+
+        /// <summary>
+        ///     Constructs a new instance of the <see cref="StringColumn" /> class with specified column name.
+        /// </summary>
+        /// <param name="columnName">Name of the colum.</param>
+        public StringColumn(string columnName) : base(columnName) { }
+
+        protected override string OnParse(string value)
+        {
+            return value;
+        }
+    }
+}
