f547dec22a796c7bf634c97bfb6704b8965545e3
Aaron Todd <aaron.todd95@gmail.com>
2018-08-23 13:19:24 +0100

    Tidied up DateTimeColumn

--- src/NeatParser/DateTimeColumn.cs
+++ src/NeatParser/DateTimeColumn.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
 
 namespace NeatParser
 {
@@ -13,21 +14,20 @@ namespace NeatParser
         ///     Constructs a new instance of the <see cref="DateTimeColumn" /> class with specified format.
         /// </summary>
         /// <param name="format">Format of the date time string e.g yyyyMMDD</param>
-        public DateTimeColumn(string format)
-        {
-            if(string.IsNullOrWhiteSpace(format))
-                throw new ArgumentNullException(nameof(format));
-
-            this.format = format;
-        }
+        /// <param name="isRequired">Is required</param>
+        public DateTimeColumn(string format, bool isRequired = false) : this(Path.GetRandomFileName(), format, isRequired) { }
 
         /// <summary>
         ///     Constructs a new instance of the <see cref="DateTimeColumn" /> class with specified format and column name.
         /// </summary>
         /// <param name="columnName">Name of the column</param>
         /// <param name="format">Format of the date time string e.g yyyyMMDD</param>
-        public DateTimeColumn(string columnName, string format) : base(columnName)
+        /// <param name="isRequired">Is required</param>
+        public DateTimeColumn(string columnName, string format, bool isRequired = false) : base(columnName, isRequired)
         {
+            if(string.IsNullOrWhiteSpace(columnName))
+                throw new ArgumentNullException(nameof(columnName));
+
             if(string.IsNullOrWhiteSpace(format))
                 throw new ArgumentNullException(nameof(format));
 
